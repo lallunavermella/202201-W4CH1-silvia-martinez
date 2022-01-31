b9a538883f5520d25a8b26e5f3ec864ec54362a9
Silvia Martinez <sil@silvia.com>
2022-01-31 21:24:15 +0100

    Update App.js with object gentlemen

--- src/App.js
+++ src/App.js
@@ -2,32 +2,26 @@ import "./App.css";
 import Info from "./components/Info";
 import Button from "./components/Button";
 import Gentlemen from "./components/Gentlemen";
+import gentlemen from "./gentlemenArray";
 
 function App() {
   const text = "0 gentlemen pointing at you";
   const infoClass = "info";
-
   const classButton = "button button--select";
   const textButton = "Select All";
 
-  const name = "El Fary";
-  const job = "Influencer";
   return (
     <>
-      <div class="container">
-        <header class="main-header">
-          <h1 class="main-title">The pointing gentlemen</h1>
+      <div className="container">
+        <header className="main-header">
+          <h1 className="main-title">The pointing gentlemen</h1>
         </header>
-        <main class="main">
-          <section class="controls">
-            <Info className={infoClass} textp={text} />
-            <Button class={classButton} text={textButton} />
-          </section>
-          <ul class="gentlemen">
-            <li class="gentleman">
-              <Gentlemen name={name} job={job} />
-            </li>
-          </ul>
+        <section className="controls">
+          <Info className={infoClass} text={text} />
+          <Button className={classButton} text={textButton} />
+        </section>
+        <main className="main">
+          <ul className="gentlemen">{gentlemen.map(Gentlemen)}</ul>
         </main>
       </div>
     </>
